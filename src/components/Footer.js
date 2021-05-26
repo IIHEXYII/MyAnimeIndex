1b4cc8c7437db58b0a52f1733ccd41c610bcb572
HeXy <57751832+IIHEXYII@users.noreply.github.com>
2021-05-26 20:08:25 +0200

    removed inactive icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AiOutlineGithub, AiFillLinkedin, AiFillTwitterCircle, AiOutlineHome } from "react-icons/ai";
+import { AiOutlineGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
 import ReactLogo from '../assets/image/round-small-hex-logo.svg';
 export default function Footer() {
 
@@ -11,8 +11,6 @@ function topFunction() {
         <>
                <footer className="footer">
                 <div className="footer__right">
-
-                    {/* <a href="https://hexyweb.com/"><AiOutlineHome className="social-icon" /></a> */}
                     <a href="https://hexyweb.com/"> <img  className="social-icon logo"src={ReactLogo} alt="React Logo" /></a>
                     <a href="https://twitter.com/IIHEXYII"><AiFillTwitterCircle className="social-icon" /></a>
                     <a href="https://www.linkedin.com/in/vetle-tessem-6039b1203/"><AiFillLinkedin className="social-icon" /></a>
