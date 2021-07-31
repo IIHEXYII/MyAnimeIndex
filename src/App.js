96cdca15d25ad01b5aa1374102e7d43da8d6b41a
Your Name <you@example.com>
2021-08-01 01:03:01 +0200

    added dropdown for anime sites

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Form from 'react-bootstrap/Form';
+// import FormControl from 'react-bootstrap/FormControl';
+// import Button from 'react-bootstrap/Button';
+
 import {
     BrowserRouter as Router,
     Switch,
@@ -19,16 +24,23 @@ const App = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='mr-auto'>
-              {/* <NavLink to='/' exact className='nav-link'>
+              {/* <NavLink to='/' exact className='nav-a'>
                 Home
               </NavLink> */}
-              {/* <NavLink to='/About' exact className='nav-link'>
+              {/* <NavLink to='/About' exact className='nav-a'>
                 Contact
               </NavLink> */}
-              <a className='nav-link' href='https://myanimelist.net/profile/-HeXy-' target='_blank' rel="noreferrer">MyAnimeList</a>
-              <a className='nav-link' href='https://anichart.net/' target='_blank' rel="noreferrer">AniChart</a>
-              <a className='nav-link' href='https://www.animenewsnetwork.com/' target='_blank' rel="noreferrer">Anime News</a>
+
+              <a className='nav-a' href='https://myanimelist.net/profile/-HeXy-' target='_blank' rel="noreferrer">MyAnimeList</a>
+              <a className='nav-a' href='https://anichart.net/' target='_blank' rel="noreferrer">AniChart</a>
+              <a className='nav-a' href='https://www.animenewsnetwork.com/' target='_blank' rel="noreferrer">Anime News</a>
+              <NavDropdown className='nav-dropdown' title="Watch Anime" id="basic-nav-dropdown">
+                <NavDropdown.Item target='_blank' rel="noreferrer" href="https://www1.gogoanime.ai/">Gogoanime</NavDropdown.Item>
+                <NavDropdown.Item target='_blank' rel="noreferrer" href="https://www1.animeultima.to/">Animeultima</NavDropdown.Item>
+                <NavDropdown.Item target='_blank' rel="noreferrer" href="https://9anime.to/">9anime</NavDropdown.Item>
+              </NavDropdown>
             </Nav>
+            
           </Navbar.Collapse>
         </Navbar>
         <Container>
